b6083c5834f1cca1d6b606d536770af914e69532
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-18 22:15:43 +0300

    Fix import errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+
+import Navbar from './Navbar';
 
 class Home extends Component {
   
   render() {
     return (
       <div className="Home">
-        this is home
+        <Navbar />
+        <Card>
+    <CardMedia
+      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+    >
+      <img src="static/homepage.jpg" alt="" />
+    </CardMedia>
+    {/* <CardTitle title="Card title" subtitle="Card subtitle" /> */}
+  </Card>
       </div>
     );
   }
