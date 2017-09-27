0c50be7207b5930a768032f465916d15217ccd15
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-27 14:38:12 +0300

    [FEATURE]Add jsx method to fix modal dialog

--- src/components/Dynamiclist.js
+++ src/components/Dynamiclist.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {Card, CardActions} from 'material-ui/Card';
 import Dialog from 'material-ui/Dialog';
 import {List, ListItem} from 'material-ui/List';
-// import { Redirect } from 'react-router-dom';
 import FlatButton from 'material-ui/FlatButton';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import TextField from 'material-ui/TextField';
@@ -24,6 +23,7 @@ class Dynamiclist extends Component {
             redirect:false,
             items:[],
             name:'',
+            // newItems : []
             
         };
       }
@@ -35,12 +35,14 @@ class Dynamiclist extends Component {
             name: event.target.value
         })
     }
-
-      getBucketItem = () => {
-          //   this gets the bucktetlist items
-          axios.get(BASE_URL + '/bucketlist/'+ this.props.bucketobj.id + '/item/', {
+    // This method gets a bucketlist's items using its id
+      getBucketItem = (id) => {
+        //   console.log('id', id)
+          axios.get(BASE_URL + '/bucketlist/'+ id + '/item/', {
               headers: {"Authorization": localStorage.getItem('token')}
           }).then((response) => {
+            //   Sets the state inclusive of that item
+            // console.log('items', response.data)
               this.setState({
                   items: response.data.item
               })
@@ -49,24 +51,36 @@ class Dynamiclist extends Component {
           })
       }
 
-      handleOpen = () => {
-        this.setState({open: true});
+    //   OPENS DIALOG FOR VIEWING BUCKETLIST TITLE AND ITS'S ITEMS USING ITS ID ie CARD_ID
+      handleOpen = (event, cardId) => {
+        event.preventDefault();
+        this.setState({
+            open: true
+        });
+
+        // Calls the method using the cardId defined
+        this.getBucketItem(cardId);
       };
 
+    //   CLOSES ALL DIALOGS
       handleClose = () => {
         this.setState({open: false, openEdit: false, openAdd: false});
       };
 
+    //   OPENS DIALOG FOR EDITING A BUCKELIST TITLE
       handleOpenEdit = () => {
         this.setState({openEdit: true});
       };
+
+    //   OPENS DIALOG FOR ADD A BUCKETLIST ITEM
       handleOpenAdd = () => {
         this.setState({openAdd: true});
       };
 
 
-    handleSubmit = () => {
-        axios.post(BASE_URL + '/bucketlist/' + this.props.bucketobj.id +'/item/', {
+    // CREATES A BUCKETLIST ITEM
+    handleAddItem = (event, id) => {
+        axios.post(BASE_URL + '/bucketlist/' + id +'/item/', {
             name: this.state.name
         }, {
             headers: {
@@ -75,18 +89,21 @@ class Dynamiclist extends Component {
             }
             // Sets the input text field to empty onSubmit
         }).then((response) => {
+            // SETS THE INPUT FIELD TO EMPTY
           this.setState({
               name: ''
           })
-          this.handleNewItem(response.data.name) 
+          //this.handleNewItem(response.data.name) 
           this.handleClose()
-          this.getBucketItem()      
+          this.getBucketItem(id)      
         })
           .catch((error) => {
             console.log(error)
           })
 
     }
+
+    // SETS THE STATE OF THE ITEMS ARRAY TO INCLUDE THE ITEM CREATED
     handleNewItem(item){
         let currentItems = this.state.items
         currentItems.push(item)
@@ -96,10 +113,7 @@ class Dynamiclist extends Component {
         })
     }
 
-    componentWillMount(){
-        this.getBucketItem()
-    }
-
+    // DISPLAYS 
     render(){
         // console.log(this.props)
         const actions = [
@@ -110,77 +124,88 @@ class Dynamiclist extends Component {
             />,
           ];
 
-          let itemsArray = []
-          if (this.state.items > 0 ){
-          itemsArray = this.state.items.map((itemObj, index) => {
-              console.log(itemObj)
-              return (
-                <Item 
-                key={index} 
-                itemObj={itemObj}
-                getBucketItem={this.getBucketItem}
-                />
-              );
-          })}
+        //   LOOPS THROUGH THE ITEMS ARRAY AND FINDS AN ITEM OBJECT
+        //   let itemsArray = []
+        //   if (this.state.items > 0 ){
         return(
             <div>
                 <List>
                     <Card>
                         <ListItem
-                            onClick={this.handleOpen}
+                            onClick={(event) => this.handleOpen(event, this.props.bucketobj.id)}
                             rightIcon={<ActionInfo />}
-                            primaryText={this.props.bucketobj.title}
-                        />
+                            primaryText={this.props.bucketobj.title}/>
+
                         <CardActions>
                             <FlatButton 
                                 label="Edit" 
                                 primary={true}
                                 onClick={this.handleOpenEdit}
-                            />
+                                />
+
                             <FlatButton 
                             label="Delete" 
                             secondary={true} 
                             onClick={() => this.props.handleDeleteBucketlist(this.props.bucketobj.id)}/>
+
                             <FlatButton
                             label="Add item"
-                            onClick={this.handleOpenAdd}
-                            />
+                            onClick={this.handleOpenAdd}/>
+
                         </CardActions>
                     </Card>  
                 </List>
       <div>
-          {/* DIALOG FOR SOWING BUCEKTLIST NAME WITH ITS ITEMS */}
+
+          {/* DIALOG FOR SHOWING BUCEKTLIST NAME WITH ITS ITEMS */}
         <Dialog
           title={this.props.bucketobj.title}
           actions={actions}
           modal={true}
           open={this.state.open}
-        >
-            <strong>{this.props.bucketobj.date_created}</strong><br/>
+          autoScrollBodyContent={true}>
+
+         <strong>{this.props.bucketobj.date_created}</strong><br/>
 
             {/* LIST SHOWS ALL BUCKEKTLIST ITEMS(VIEW) */}
             <List>
-                {itemsArray.length > 0  ? itemsArray: 'No items yet'}
+                {this.state.items === undefined ? "No Items Yet":
+                
+                    this.state.items.map((itemObj, index) => {
+                        return (
+                            <Item 
+                            key={index} 
+                            itemObj={itemObj}
+                            getBucketItem={this.getBucketItem}/>
+                        );
+                    })
+                }
+                {/* {itemsArray.length > 0  ? itemsArray: 'No items yet'} */}
             </List>
         </Dialog>
 
-        {/* DIALOG FOR CREATING A BUCKETLIST */}
+        {/* DIALOG FOR UPDATING A BUCKETLIST */}
         <Dialog
           title={this.props.bucketobj.title}
           actions={actions}
           modal={true}
           open={this.state.openEdit}
-        >
-        <strong>{this.props.bucketobj.date_modified}</strong><br/>
+          autoScrollBodyContent={true}>
+    
+          <strong>{this.props.bucketobj.date_modified}</strong><br/>
             <TextField
                 name="title"
                 hintText="Name your bucket"
-                onChange={this.props.handleChange}
-            />
+                onChange={this.props.handleChange}/>
+
                 <FlatButton 
                 type="submit" 
                 label="Submit" primary={true} 
-                onClick={() => this.props.handleUpdateBucketlist(this.props.bucketobj.id)}/>
+                onClick={() => {
+                    this.props.handleUpdateBucketlist(this.props.bucketobj.id)
+                    this.handleClose()
+                }}/>
+
         </Dialog>
 
         {/* DIALOG FOR ADDING A BUCKETLIST ITEM */}
@@ -188,21 +213,23 @@ class Dynamiclist extends Component {
           title={this.props.bucketobj.title}
           actions={actions}
           modal={true}
-          open={this.state.openAdd}>
+          open={this.state.openAdd}
+          autoScrollBodyContent={true}>
+
           <TextField
             name="name"
             hintText="Your activites"
-            onChange={this.handleChange}
-            />
+            onChange={this.handleChange}/>
+
             <FlatButton
                 type="submit"
                 label="submit" primary={true}
 
-                onClick={this.handleSubmit}
-            />
-        </Dialog>
+                onClick={(event) => this.handleAddItem(event, this.props.bucketobj.id)}/>
 
-      </div></div>
+        </Dialog>
+        </div>
+        </div>
         );
     }
 }
