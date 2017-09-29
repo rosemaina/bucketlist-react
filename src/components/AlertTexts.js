71f37c862d7fb92d5a5eeed6e382be5dc809a5ee
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-29 14:48:45 +0300

    [FEATURE]Add class for sending alerts

---
+++ src/components/AlertTexts.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+
+export default class AlertTexts extends Component {
+
+  render(){
+      return (
+        <div>
+        <ToastContainer
+          position="bottom-right"
+          type="default"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnHover
+        />
+        </div>
+      );
+    }
+}
