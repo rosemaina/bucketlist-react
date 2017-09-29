846197920b16515de45e4b9b7560f8f2ec531e50
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-29 14:54:44 +0300

    [CHORE]Remove commented code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,41 +5,19 @@ import { Link } from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 
 
-// class Navbar extends Component {
-  
-//   render() {
-//     return (
-//       <div className="Navbar">
-//         <AppBar
-//           title="BucketList App"
-//           iconElementRight={
-//             <div>
-//             <Link to={'/login'}><FlatButton label="Login" /></Link>
-//             <Link to={'/register'}><RaisedButton label="Sign up" primary={true} /></Link>
-//             <Link to={'/logout'}><FlatButton label="Logout" secondary={true} /></Link>
-//           </div>
-//           }
-          
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-const Navbar = ({navBarTitle}) => {
-
+const Navbar = (props) => {
   
   return (
     <div className="Navbar">
     <AppBar
       showMenuIconButton={false}
-      title={navBarTitle}
+      title={props.navBarTitle}
       iconElementRight={
         <div>
         <Link to={'/login'}><FlatButton label="Login" /></Link>
-        <Link to={'/register'}><RaisedButton label="Sign up" primary={true} /></Link>
+        <Link to={'/register'}><RaisedButton label="Sign up" primary={true} /></Link><br/>
+        <RaisedButton label="Log Out" primary={true} onClick={props.logout}/>
       </div>
-      
       }
       
     />
