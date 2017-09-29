645bcc39d24d44a41222d3fdf605958e30b74a1c
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-29 14:47:20 +0300

    [FEATURE]Add search function

--- src/components/Addbucket.js
+++ src/components/Addbucket.js
@@ -4,23 +4,29 @@ import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 
 
-
 const Addbucket = (props) => {
         return(
             <div>
+                <TextField 
+                    placeholder="Search"
+                    name="Search"
+                    onClick={props.handleSearch}
+                    />
+                    <button>Search</button><br/>
                 <Card >
                     <form onSubmit={props.newBucketlist}>
                         <TextField
-                            placeholder="Name"
+                            placeholder="Name your bucketlist"
                             name="title"
                             onChange={props.handleChange}
-                            floatingLabelFixed={true}
+                            floatingLabelFixed
                             value={props.title}
                         />
-                        <FlatButton type="submit" label="Create" primary={true} />
+                        <FlatButton label= "Create" primary type="submit" />
                     </form>
                 </Card>
             </div>
         );
-    }
+    };
+
 export default Addbucket;
