4fd032de5f9ca5dd891e4c8b3e65ae58f9f292d8
Ace <rossie@Roses-MacBook-Pro.local>
2017-09-29 14:55:22 +0300

    [FEATURE]Add logout function

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Card, CardTitle, CardText, CardMedia} from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
-import { Redirect } from 'react-router-dom';
-
+import { Redirect, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import AlertTexts from './AlertTexts';
 
 import Navbar from './Navbar';
 
+
 const axios = require('axios')
+
 
 class Login extends Component {
     constructor(props){
@@ -15,37 +18,47 @@ class Login extends Component {
         this.state={
             email:'',
             password:'',
-            login_success: false
+            login_success: false,
+            logout_success: false
         }
     }
 
+    // Method changes state for every instance of input
     handleChange = (event) => {
         const name = event.target.name
         this.setState({[name]: event.target.value})
     }
 
+    // Method login a user and gives them a token
     login = (event) => {
         event.preventDefault()
         axios.post('http://127.0.0.1:5000/auth/login', {
             email: this.state.email,
             password: this.state.password
-          })
-          .then(resp => {
+          }).then(resp => {
               console.log(resp)
               localStorage.setItem('token', resp.data['token']);
           })
-          .then(() => this.setState({login_success: true }))
+          .then((resp) => this.setState({login_success: true }))
           .catch((error) => {
-            // this.setState({error: error.response.data.error})
-            // this.setState({open: true})
-            console.log(error)
+            toast.error(error.response.data.error)
+             
           })
+    }
+
+    // Methods logs out a user
+    handleLogout =(event) =>{
+        console.log('Clicked')
+        event.preventDefault()
+        localStorage.removeItem('token');
+        this.setState({
+            logout_success: true
+        })
     }
 
     render() {
         const style = {
             margin: "auto", 
-            // margin: "center",
             width: "50%",
             height: "auto",
             textAlign: 'center',
@@ -56,16 +69,13 @@ class Login extends Component {
             top: "0%",
             right: "0%",
             left: "0%",
-            background: "rgba(0, 0, 0, 0.44)",
+            background: "rgba(0, 0, 0, 0.34)",
         }
 
         const titleStyle ={
             color: "rgba(255, 255, 255, 0.87)",
         }
 
-        const color={
-            color: "rgba(255, 255, 255, 0.87)",
-        }
         const styles={
             hintColor: {
                 color: "rgba(255, 255, 255, 0.87)",
@@ -76,18 +86,23 @@ class Login extends Component {
         }
 
 
-
-
+        // Redirections Conditions
         if (this.state.login_success) {
             return(
                 <Redirect to='/bucketlist' />
+            );
+        }
+
+        if (this.state.logout_success) {
+            return(
+                <Redirect to='/login' />
             );
         }
         return (
             <div>
                 
             <Navbar 
-            navBarTitle="BucketListly Adventure"
+            navBarTitle="BucketListy Adventure"
             />
             <Card >
                 <CardMedia
@@ -99,7 +114,7 @@ class Login extends Component {
                             <CardTitle 
                             titleStyle={titleStyle}
                             title="Login here"/>
-                            <CardText color={color}>
+                            <CardText>
                                 <form onSubmit={this.login}>
                                     <TextField 
                                         name="email"
@@ -112,7 +127,6 @@ class Login extends Component {
                                     /><br />
 
                                     <TextField
-                                        color="white"
                                         name="password"
                                         hintText="password"
                                         hintStyle={styles.hintColor}
@@ -122,22 +136,23 @@ class Login extends Component {
                                         onChange = {this.handleChange}
                                     /><br />
                                     <RaisedButton type="submit" label="login" primary={true}/>
+                                    <br/><br/>
+                                    <Link to={'/changepassword'}>
+                                        forgot password?
+                                    </Link>
                                 </form>
                             </CardText>
                         </div>
                     }
                     overlayContentStyle={overlayContentStyle}
                     >
-                    {/* static/clouds.jpg */}
-                    <img src="static/homepage.jpg" alt=""/>
+                    <img src="static/clouds.jpg" alt=""/>
                 </CardMedia>
             </Card>
+            <AlertTexts />
             </div>
         );
   }
 }
-// const style = {
-//     margin: 15,
-//    };
 
 export default Login;
